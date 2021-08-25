f9625c3c21347f19fa2387dd0323d390744543d7
Vinicius Solon <vini.97@live.com>
2021-08-25 07:59:21 -0300

    Implementado profile de testes com banco H2 e adicionado anotação @Entity JPA na entidade User para mapeamento

--- src/main/java/com/vinisolon/orders/domain/User.java
+++ src/main/java/com/vinisolon/orders/domain/User.java
@@ -6,17 +6,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+// Lombok
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = {"id"})
+@EqualsAndHashCode(of = "id")
+// JPA
+@Entity
 public class User implements Serializable {
 
     private static final long serialVersionUID = -2244715368275881723L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
