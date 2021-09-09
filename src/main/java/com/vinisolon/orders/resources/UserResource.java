3939e0e2f8b40fbda438594d050eb13a9ff5ac84
Vinicius Solon <vini.97@live.com>
2021-09-09 18:29:14 -0300

    Implementado insert de User com retorno 201 utilizando URI

--- src/main/java/com/vinisolon/orders/resources/UserResource.java
+++ src/main/java/com/vinisolon/orders/resources/UserResource.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,6 +32,15 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        // É esperando retorno 201 em requisição HTTP de POST
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        // O método created espera um cabeçalho do tipo location contendo o endereço do novo recurso inserido, a URI
+        return ResponseEntity.created(uri).body(obj);
     }
 
 }
